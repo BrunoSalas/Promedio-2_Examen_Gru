e728768ff04c2792875c54abd91ca166cf65c3c8
iDinoDave <97319831+iDinoDave@users.noreply.github.com>
2023-05-25 09:52:17 -0500

    David Achachagua
    Clase abstracta
    Interfaz
    Singleton
    Observer
    Action

--- My project/Assets/Scripts/SpawnNivel1.cs
+++ My project/Assets/Scripts/SpawnNivel1.cs
@@ -1,30 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
 
-public class SpawnNivel1 : MonoBehaviour
+public class SpawnNivel1 : MonoBehaviour , IObserver
 {
     public float timer;
     public GameObject spawnPrefabEnemy;
-    
-    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        Timer.GetInstance().Attach(this);
+    }
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;
+    }
 
-        if (timer > 2)    
-        {
-
-            Instantiate(spawnPrefabEnemy, transform.parent.position + transform.localPosition, transform.parent.rotation);
-            timer= 0;
-
-        }
+    public void Execute(ISubject subject)
+    {
+        Instantiate(spawnPrefabEnemy, transform.parent.position + transform.localPosition, transform.parent.rotation);
     }
 
 
--- My project/Assets/Scripts/Timer.cs
+++ My project/Assets/Scripts/Timer.cs
@@ -4,7 +4,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Timer : MonoBehaviour
+public class Timer : MonoBehaviour , ISubject
 {
     [SerializeField]
     private TMP_Text tiempoText;
@@ -14,6 +14,18 @@ public class Timer : MonoBehaviour
 
     private float currentTime;
 
+    public static Timer instance;
+    public float timer;
+    public IObserver spawn;
+    private void Awake()
+    {
+        instance = this;
+    }
+    public static Timer GetInstance()
+    {
+        return instance;
+    }
+
     private void Start()
     {
         currentTime = initialTime;
@@ -22,6 +34,7 @@ public class Timer : MonoBehaviour
     private void Update()
     {
         currentTime -= Time.deltaTime;
+        timer += Time.deltaTime;
 
         if (currentTime <= 0f)
         {
@@ -29,8 +42,27 @@ public class Timer : MonoBehaviour
             currentTime = 0f;
 
         }
+        if (timer > 2)
+        {
+            Notify();
+            timer = 0;
+        }
 
         tiempoText.text = $"Tiempo: {currentTime.ToString("0")}";
     }
 
+    public void Notify()
+    {
+        spawn.Execute(this);
+    }
+
+    public void Attach(IObserver observer)
+    {
+        spawn = observer;
+    }
+
+    public void Remove(IObserver observer)
+    {
+
+    }
 }
